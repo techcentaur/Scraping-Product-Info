c4f03b4884752c6ef128951e625e4efcce613132
Ankit Solanki <ankit03june@gmail.com>
2018-07-14 18:49:07 +0530

    [INITIAL] setup for basic webapp for scraping stuff.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import flash, redirect, render_template, request, session, abort
 
 from scraper import Scraper
+from amazon_scraper import AmazonScraper
+
 
 app = Flask(__name__)
 
@@ -9,18 +11,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/info", methods=['GET'])
+@app.route("/info/<link>", methods=['GET'])
 def info(link):
     s = Scraper(link)
     data = s.get_data()
 
+    if data.status:
+        pass
+    else:
+        flash('Incorrect link! Please insert a correct one.')
+        return redirect(url_for('index'))
+
     return render_template('product.html', data = data)
-
-@app.route("/error", methods=['GET'])
-def error():
-    
-    return render_template('error.html')
-
 
 if __name__=="__main__":
     app.run(debug=True)
