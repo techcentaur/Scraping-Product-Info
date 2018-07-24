c2a5f7f89a485da05d1435417b899bd4d55eec98
Ankit Solanki <ankit03june@gmail.com>
2018-07-24 14:50:47 +0530

    [MODIFIED] work - application developed and requiements added

--- scraper.py
+++ scraper.py
@@ -15,7 +15,6 @@ class Amazon:
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'}
         page = requests.get(url, headers=headers)
         
-        sleep(3)            
         try:
             doc = html.fromstring(page.content)
             XPATH_NAME = '//h1[@id="title"]//text()'
@@ -23,19 +22,22 @@ class Amazon:
             XPATH_ORIGINAL_PRICE = '//td[contains(text(),"List Price") or contains(text(),"M.R.P") or contains(text(),"Price")]/following-sibling::td/text()'
             XPATH_CATEGORY = '//a[@class="a-link-normal a-color-tertiary"]//text()'
             XPATH_AVAILABILITY = '//div[@id="availability"]//text()'
- 
+            XPATH_ETA = '//div[@id="ddmDeliveryMessage"]//text()'
+
             RAW_NAME = doc.xpath(XPATH_NAME)
             RAW_SALE_PRICE = doc.xpath(XPATH_SALE_PRICE)
             RAW_CATEGORY = doc.xpath(XPATH_CATEGORY)
             RAW_ORIGINAL_PRICE = doc.xpath(XPATH_ORIGINAL_PRICE)
             RAW_AVAILABILITY = doc.xpath(XPATH_AVAILABILITY)
- 
+            RAW_ETA = doc.xpath(XPATH_ETA)
+
             NAME = ' '.join(''.join(RAW_NAME).split()) if RAW_NAME else None
             SALE_PRICE = ' '.join(''.join(RAW_SALE_PRICE).split()).strip() if RAW_SALE_PRICE else None
             CATEGORY = ' > '.join([i.strip() for i in RAW_CATEGORY]) if RAW_CATEGORY else None
             ORIGINAL_PRICE = ''.join(RAW_ORIGINAL_PRICE).strip() if RAW_ORIGINAL_PRICE else None
             AVAILABILITY = ''.join(RAW_AVAILABILITY).strip() if RAW_AVAILABILITY else None
- 
+            ETA = ' '.join(''.join(RAW_ETA).split()) if RAW_ETA else None
+
             if not ORIGINAL_PRICE:
                 ORIGINAL_PRICE = SALE_PRICE
 
@@ -47,6 +49,7 @@ class Amazon:
                     'ORIGINAL PRICE': ORIGINAL_PRICE,
                     'AVAILABILITY': AVAILABILITY,
                     'URL': url,
+                    'ETA': ETA
                     }
  
             return data
