30f72cffc91938295756faaceffeb40584cffff4
Ankit Solanki <ankit03june@gmail.com>
2018-09-01 10:56:38 +0530

    [EDIT] scraper for support of ebay URL with bestsellers

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import re
 import pprint
 import requests
-from lxml import html  
+from lxml import (html, etree) 
 from time import sleep
 from collections import OrderedDict
 
@@ -15,79 +15,87 @@ class Amazon:
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'}
         page = requests.get(url, headers=headers)
         
+        # try:
+        doc = html.fromstring(page.content)
+        XPATH_NAME = '//h1[@id="title"]//text()'
+        XPATH_SALE_PRICE = '//span[contains(@id,"ourprice") or contains(@id,"saleprice")]/text()'
+        XPATH_ORIGINAL_PRICE = '//td[contains(text(),"List Price") or contains(text(),"M.R.P") or contains(text(),"Price")]/following-sibling::td/text()'
+        XPATH_CATEGORY = '//a[@class="a-link-normal a-color-tertiary"]//text()'
+        XPATH_AVAILABILITY = '//div[@id="availability"]//text()'
+        XPATH_ETA = '//div[@id="ddmDeliveryMessage"]//text()'
+        buyop = '//div[@class="a-text-center a-spacing-mini"]//text()'
+        delop = '//div[@id="contextualIngressPt"]//text()'
+        prodinfo = '//div[@class="content"]//text()'
+
+
+        RAW_NAME = doc.xpath(XPATH_NAME)
+        RAW_SALE_PRICE = doc.xpath(XPATH_SALE_PRICE)
+        RAW_CATEGORY = doc.xpath(XPATH_CATEGORY)
+        RAW_ORIGINAL_PRICE = doc.xpath(XPATH_ORIGINAL_PRICE)
+        RAW_AVAILABILITY = doc.xpath(XPATH_AVAILABILITY)
+        RAW_ETA = doc.xpath(XPATH_ETA)
+
         try:
-            doc = html.fromstring(page.content)
-            XPATH_NAME = '//h1[@id="title"]//text()'
-            XPATH_SALE_PRICE = '//span[contains(@id,"ourprice") or contains(@id,"saleprice")]/text()'
-            XPATH_ORIGINAL_PRICE = '//td[contains(text(),"List Price") or contains(text(),"M.R.P") or contains(text(),"Price")]/following-sibling::td/text()'
-            XPATH_CATEGORY = '//a[@class="a-link-normal a-color-tertiary"]//text()'
-            XPATH_AVAILABILITY = '//div[@id="availability"]//text()'
-            XPATH_ETA = '//div[@id="ddmDeliveryMessage"]//text()'
-            buyop = '//div[@class="a-text-center a-spacing-mini"]//text()'
-            delop = '//div[@id="contextualIngressPt"]//text()'
-            prodinfo = '//div[@class="content"]//text()'
-
-
-            RAW_NAME = doc.xpath(XPATH_NAME)
-            RAW_SALE_PRICE = doc.xpath(XPATH_SALE_PRICE)
-            RAW_CATEGORY = doc.xpath(XPATH_CATEGORY)
-            RAW_ORIGINAL_PRICE = doc.xpath(XPATH_ORIGINAL_PRICE)
-            RAW_AVAILABILITY = doc.xpath(XPATH_AVAILABILITY)
-            RAW_ETA = doc.xpath(XPATH_ETA)
-
-            try:
-                buyopval = doc.xpath(buyop)
-                buyopval = " ".join(buyopval)
-                buyopval = buyopval.replace('\xa0', '')
-            except Exception as e:
-                buyopval = None
-
-            try:
-                delopval = doc.xpath(delop)
-                _del = ' '.join(''.join(delopval).split()) if delopval else None
-            except Exception as e:
-                _del = None
-
-            ll = []
-            try:
-                raw_prodinfo = doc.xpath(prodinfo)
-                l = raw_prodinfo
-                l = ' '.join(''.join(l).split()) if l else None
-                l = re.sub(r'\{[^}]*\}', '', l)
-                l = l.split()
-                for i in l:
-                    if not i.startswith('.'):
-                        ll.append(i)
-                ll = " ".join(ll)
-            except Exception as e:
-                ll = "None available"
-
-            NAME = ' '.join(''.join(RAW_NAME).split()) if RAW_NAME else None
-            SALE_PRICE = ' '.join(''.join(RAW_SALE_PRICE).split()).strip() if RAW_SALE_PRICE else None
-            CATEGORY = ' > '.join([i.strip() for i in RAW_CATEGORY]) if RAW_CATEGORY else None
-            ORIGINAL_PRICE = ''.join(RAW_ORIGINAL_PRICE).strip() if RAW_ORIGINAL_PRICE else None
-            AVAILABILITY = ''.join(RAW_AVAILABILITY).strip() if RAW_AVAILABILITY else None
-            ETA = ' '.join(''.join(RAW_ETA).split()) if RAW_ETA else None
-
-            if not ORIGINAL_PRICE:
-                ORIGINAL_PRICE = SALE_PRICE
-
-            data = OrderedDict()
-            data = {
-                    'NAME': NAME,
-                    'SALE PRICE': SALE_PRICE,
-                    'CATEGORY': CATEGORY,
-                    'ORIGINAL PRICE': ORIGINAL_PRICE,
-                    'AVAILABILITY': AVAILABILITY,
-                    'URL': url,
-                    'ETA': ETA,
-                    'BUY OPTIONS': buyopval + " | " + _del,
-                    'Product Info': ll
-                    }
- 
-            return data
+            buyopval = doc.xpath(buyop)
+            buyopval = " ".join(buyopval)
+            buyopval = buyopval.replace('\xa0', '')
         except Exception as e:
-            return {"Exception": e}
+            buyopval = None
+
+        try:
+            delopval = doc.xpath(delop)
+            _del = ' '.join(''.join(delopval).split()) if delopval else None
+        except Exception as e:
+            _del = None
+
+        ll = []
+        try:
+            raw_prodinfo = doc.xpath(prodinfo)
+            l = raw_prodinfo
+            l = ' '.join(''.join(l).split()) if l else None
+            l = re.sub(r'\{[^}]*\}', '', l)
+            l = l.replace("P.when(\"jQuery\", \"a-popover\", \"ready\").execute(function ($, popover) ); });", '')
+            l = l.split()
+            for i in l:
+                if not i.startswith('.'):
+                    ll.append(i)
+            ll = " ".join(ll)
+            print(l)
+        except Exception as e:
+            ll = "None available"
+
+        NAME = ' '.join(''.join(RAW_NAME).split()) if RAW_NAME else None
+        SALE_PRICE = ' '.join(''.join(RAW_SALE_PRICE).split()).strip() if RAW_SALE_PRICE else None
+        CATEGORY = ' > '.join([i.strip() for i in RAW_CATEGORY]) if RAW_CATEGORY else None
+        ORIGINAL_PRICE = ''.join(RAW_ORIGINAL_PRICE).strip() if RAW_ORIGINAL_PRICE else None
+        AVAILABILITY = ''.join(RAW_AVAILABILITY).strip() if RAW_AVAILABILITY else None
+        ETA = ' '.join(''.join(RAW_ETA).split()) if RAW_ETA else None
+
+        if not ORIGINAL_PRICE:
+            ORIGINAL_PRICE = SALE_PRICE
+
+        if buyopval is None:
+            buyopval = ' '
+        if _del is None:
+            _del = ' '
+
+        data = OrderedDict()
+        data = {
+                'NAME': NAME,
+                'SALE PRICE': SALE_PRICE,
+                'CATEGORY': CATEGORY,
+                'ORIGINAL PRICE': ORIGINAL_PRICE,
+                'AVAILABILITY': AVAILABILITY,
+                'URL': url,
+                'ETA': ETA,
+                'BUY OPTIONS': buyopval + " | " + _del,
+                'Additional Product Info': ll
+                }
+
+        # print(data)
+        return data
+        # except Exception as e:
+            # return {"Exception": e}
      
 
     # If ASIN given (Amazon specific)
@@ -97,13 +105,31 @@ class Amazon:
      
 
 class eBay:
-    """Ebay scraper"""
+    """ Ebay scraper with `requests` """
     def __init__(self):
         pass
 
     def eBay_parser(self, link):
+        linkparts = link.split('/')
+        print(linkparts)
+        if linkparts[3] == 'p':
+            resp = requests.get(link)
+            docresp = html.fromstring(resp.content)
+            for linkele in docresp.xpath('//div[contains(@class, "item-details")]//a'):
+                href = linkele.get('href')
+                e = self.ebay_parser_with_itm_rest_url(href)
+
+        elif linkparts[3] == 'itm':
+            e = self.ebay_parser_with_itm_rest_url(link)
+        else:
+            print("Invalid Link Found")
+            e = {}
+        return e
+
+    def ebay_parser_with_itm_rest_url(self, link):
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'}
         page = requests.get(link, headers=headers)
+        
         try:
             doc = html.fromstring(page.content)
             TITLE = '//h1[@id="itemTitle"]//text()'
@@ -159,8 +185,7 @@ class eBay:
             print(e)
         return True
 
-
 if __name__=="__main__":
-    a = Amazon().Amazon_parser("https://www.amazon.in/Esquire-Spin-mop-2-Refills/dp/B071JWBFDT/ref=lp_15185218031_1_2?s=home-improvement&ie=UTF8&qid=1532165388&sr=1-2")
+    link = input()
     e = eBay().eBay_parser("https://www.ebay.in/itm/portable-rugby-wireless-bluetooth-mini-stereo-speaker-fm-radio-usb-microsd/292105966607?hash=item4402df540f")
-    
+    print(e)    
